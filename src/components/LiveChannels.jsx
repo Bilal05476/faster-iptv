864c546fb79c2cf42466aca2172cb43eb6936815
bilal05476 <ba4762313@gmail.com>
2022-10-16 17:07:11 +0500

    updates

--- src/components/LiveChannels.jsx
+++ src/components/LiveChannels.jsx
@@ -4,78 +4,98 @@ import tv from "../assets/icons/tv.png";
 import star from "../assets/icons/star.png";
 import channelIcon from "../assets/icons/channel.png";
 import channelImage from "../assets/images/channelimage.png";
+import play from "../assets/images/playerscreen.png";
+import { NavLink } from "react-router-dom";
 
 const LiveChannels = () => {
+  const [view, setView] = useState(1);
   const [channel, setChannel] = useState({
     img: channelImage,
     cName: "MBC Channel",
     quality: "1 MBC > FHD",
     duration: "2:33 pm",
   });
-  return (
-    <div className="liveChannels">
-      <div className="header">
-        <img src={tv} alt="tv-channels" />
-        <h4>Live Channels</h4>
-      </div>
-      <div className="content">
-        <div className="channel-list">
-          <Channel
-            channelName={"MBC Channel"}
-            channelQuality={`1 MBC > FHD`}
-            setChannel={setChannel}
-          />
-          <Channel
-            channelName={"ABC"}
-            channelQuality={`2 MBC > FHD`}
-            setChannel={setChannel}
-          />
-          <Channel
-            channelName={"BBC"}
-            channelQuality={`3 MBC > FHD`}
-            setChannel={setChannel}
-          />
-          <Channel
-            channelName={"CNN"}
-            channelQuality={`4 MBC > FHD`}
-            setChannel={setChannel}
-          />
-          <Channel
-            channelName={"News A"}
-            channelQuality={`5 MBC > FHD`}
-            setChannel={setChannel}
-          />
-          <Channel
-            channelName={"News B"}
-            channelQuality={`6 MBC > FHD`}
-            setChannel={setChannel}
-          />
-          <Channel
-            channelName={"News B"}
-            channelQuality={`7 MBC > FHD`}
-            setChannel={setChannel}
-          />
-          <Channel
-            channelName={"News C"}
-            channelQuality={`8 MBC > FHD`}
-            setChannel={setChannel}
-          />
-          <Channel
-            channelName={"News D"}
-            channelQuality={`9 MBC > FHD`}
-            setChannel={setChannel}
-          />
-        </div>
-        <div className="channel-details">
-          <img className="channel-img" src={channel.img} alt="channel" />
 
-          <ChannelDetails
-            channelName={channel.cName}
-            channelQuality={channel.quality}
-            channelDuration={channel.duration}
-          />
+  return (
+    <div>
+      {view === 1 && (
+        <div className="liveChannels">
+          <div className="header">
+            <img src={tv} alt="tv-channels" />
+            <h4>Live Channels</h4>
+          </div>
+          <div className="content">
+            <div className="channel-list">
+              <Channel
+                channelName={"MBC Channel"}
+                channelQuality={`1 MBC > FHD`}
+                setChannel={setChannel}
+              />
+              <Channel
+                channelName={"ABC"}
+                channelQuality={`2 MBC > FHD`}
+                setChannel={setChannel}
+              />
+              <Channel
+                channelName={"BBC"}
+                channelQuality={`3 MBC > FHD`}
+                setChannel={setChannel}
+              />
+              <Channel
+                channelName={"CNN"}
+                channelQuality={`4 MBC > FHD`}
+                setChannel={setChannel}
+              />
+              <Channel
+                channelName={"News A"}
+                channelQuality={`5 MBC > FHD`}
+                setChannel={setChannel}
+              />
+              <Channel
+                channelName={"News B"}
+                channelQuality={`6 MBC > FHD`}
+                setChannel={setChannel}
+              />
+              <Channel
+                channelName={"News B"}
+                channelQuality={`7 MBC > FHD`}
+                setChannel={setChannel}
+              />
+              <Channel
+                channelName={"News C"}
+                channelQuality={`8 MBC > FHD`}
+                setChannel={setChannel}
+              />
+              <Channel
+                channelName={"News D"}
+                channelQuality={`9 MBC > FHD`}
+                setChannel={setChannel}
+              />
+            </div>
+            <div className="channel-details">
+              <img
+                className="channel-img"
+                src={channel.img}
+                alt="channel"
+                onClick={() => setView(2)}
+              />
+              <ChannelDetails
+                channelName={channel.cName}
+                channelQuality={channel.quality}
+                channelDuration={channel.duration}
+              />
+            </div>
+          </div>
         </div>
-      </div>
+      )}
+      {view === 2 && (
+        <div className="playChannels">
+          <span className="play-back" onClick={() => setView(1)}>
+            Back
+          </span>
+          <img src={play} alt="play screen" />
+        </div>
+      )}
     </div>
   );
 };
